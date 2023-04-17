474f0d84da77e9aa3f04b880fda37e613f952222
Joseph Teklu <jteklu@outlook.com>
2023-04-16 18:59:22 -0600

    updated npm

--- index.js
+++ index.js
@@ -3,6 +3,22 @@
 // TODO: Create an array of questions for user input
 const questions = [];
 
+var inquirer = require('inquirer');
+inquirer
+  .prompt([{
+    type: "input",
+    message: 'what is your name',
+    name: "name",
+  }])
+  .then((answers) => {console.log(answers)})
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
