a9011b2729e6c4a755349e37ad1c13ffb3a45ee6
Joseph <jteklu@outlook.com>
2023-04-18 14:09:08 -0600

    finished the questions script and fstream to read file

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 // write the questions in a file and output them one by one
-const questions = ["What is the name of your project?", "Make sure your title is 3 or less words that describe your project."];
+const questions = [];
 
 function askQuestions(){
-  
-  // welcome the user to the application
-  console.log("\n---------------------------------")
-  console.log("   Welcome to README Generator\n");
+  const fs = require("fs");
+  const readline = require("readline");
+  const text = fs.createReadStream("./questions.txt")
 
-  for (let i = 0; i < questions.length; i+2) {
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: `${questions[0]} \n ${questions[1]}`,
-        name: 'project name',
-      },
-    ])
-    .then((response) =>{
-      // create the readme file and add it here
-    });
+  // populate the array with the questions from questions.txt
+  const rl = readline.createInterface({
+    input: text,
+  })
+  rl.on("line",(lineText)=> {
+    questions.push(toString(lineText));
+  })
+  console.log("here" + questions.length)
+  for (let i = 0; i < questions.length; i++) {
+    console.log("here");
+    console.log(questions[i] + ":" + i)
+    
   }
+
+
+
+  // // welcome the user to the application
+  // console.log("\n---------------------------------")
+  // console.log("   Welcome to README Generator\n");
+
+  // for (let i = 0; i < questions.length; i+2) {
+  //   inquirer
+  //   .prompt([
+  //     {
+  //       type: 'input',
+  //       message: `${questions[0]} \n ${questions[1]}`,
+  //       name: 'project name',
+  //     },
+  //   ])
+  //   .then((response) =>{
+  //     // create the readme file and add it here
+  //   });
+  // }
 
 }
 
