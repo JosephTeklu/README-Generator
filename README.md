58b12fb656abbe3befed13629921c88a33b6453a
Joseph Teklu <jteklu@outlook.com>
2023-04-25 21:28:28 -0600

    finised product with created readme using actual product

---
+++ README.md
@@ -0,0 +1,24 @@
+# README-Generator
+  MIT<br>[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description 
+  This generator was created as part of my homework but also as a way for me to create a readme for any project in simple easy steps. It allows me to not have to go through the trouble of writing markdown but answering simple questions that will generate me a functional readme.
+## Table of Contents
+  - [Installation](#installation)
+ - [Usage](#usage) 
+ - [Contributing Guidlines](#contributing) 
+ - [Tests](#tests) 
+ - [Questions](#questions) 
+ - [License](#license)
+## Installation 
+  Clone the repository to your local machine. In the directory run (npm i -y) then node (index.js) to run the program
+## Usage 
+  Answer the prompts about each section of the readme. This program also includes some discriptions of what to add in each section as a helping guide. Then give the readme a name and you're all set.
+## Contributing 
+  Right now no contribution is necessary.
+## Tests 
+  Check out the code and change what you dont like. I would like to add more options with the table of contents possibly you can do that on your own.
+## Questions
+  Contact me at my github or my email for further questions: https://github.com/JosephTeklu jteklu@outlook.com
+## License
+   License under MIT
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,13 +9,19 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  // change choices to match what was asked
+  // add in the data enterd by user as well as calling to get api badge
   return `# ${data.title}
+  ${data.license === 
+    "Apache License 2.0" ? "Apache License 2.0" + "" + '<br>' + "" + "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.license === "MIT" ? "MIT" + "" + '<br>' + "" + "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.license === "IBM" ? "IBM" + "" + '<br>' + "" + "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)" : "Perl" + "" + '<br>' + "" + "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"}
 ## Description 
   ${data.description}
 ## Table of Contents
- ${data.toc === 
-" - [Installation](#installation)\n - [Usage](#usage)\n - [Contributing Guidlines](#contributing)\n - [Tests](#tests)\n - [Questions](#questions)\n - [License](#License)"}
+  - [Installation](#installation)
+ - [Usage](#usage) 
+ - [Contributing Guidlines](#contributing) 
+ - [Tests](#tests) 
+ - [Questions](#questions) 
+ - [License](#license)
 ## Installation 
   ${data.installation}
 ## Usage 
@@ -25,10 +31,9 @@ function generateMarkdown(data) {
 ## Tests 
   ${data.tests}
 ## Questions
-  ${data.questions}
+  Contact me at my github or my email for further questions: https://github.com/${data.questions}
 ## License
-  ${data.license === 
-  "Apache License 2.0" ? "Apache License 2.0" + "" + '<br>' + "" + "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.license === "MIT" ? "MIT" + "" + '<br>' + "" + "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.license === "IBM" ? "IBM" + "" + '<br>' + "" + "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)" : "Perl" + "" + '<br>' + "" + "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"}
-`;
+   License under ${data.license}
+  `;
 }
 module.exports = generateMarkdown;
